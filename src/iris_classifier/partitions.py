ff00c2f423c3dac01179dd8b984e9a3c129ba00c
Juan Bernardo Benavides Rubio <juan.benavides@fintegra.co>
2022-12-28 16:36:46 -0500

    :sparkles: Add higher order function for partitioning data

--- src/iris_classifier/partitions.py
+++ src/iris_classifier/partitions.py
@@ -3,9 +3,15 @@ from __future__ import annotations
 
 import abc
 import random
-from typing import Iterable, List, overload
+from typing import Iterable, List, overload, Callable
 
-from .samples import SampleDict, TestingKnownSample, TrainingKnownSample, KnownSample
+from .samples import (
+    SampleDict,
+    TestingKnownSample,
+    TrainingKnownSample,
+    KnownSample,
+    Sample,
+)
 
 
 class SamplePartition(List[SampleDict], abc.ABC):
@@ -140,3 +146,41 @@ class CountingDealingPartition(DealingPartition):
     @property
     def testing(self) -> list[TestingKnownSample]:
         return self._testing
+
+
+def training_80(s: Sample, i: int) -> bool:
+    return i % 5 != 0
+
+
+def training_90(s: Sample, i: int) -> bool:
+    return i % 10 != 0
+
+
+def training_75(s: Sample, i: int) -> bool:
+    return i % 4 != 0
+
+
+def training_67(s: Sample, i: int) -> bool:
+    return i % 3 != 0
+
+
+TrainingList = list[TrainingKnownSample]
+TestingList = list[TestingKnownSample]
+
+
+def partition_samples(
+    samples: Iterable[SampleDict],
+    rule: Callable[[KnownSample, int], bool],
+) -> tuple[TrainingList, TestingList]:
+    """Partition samples into training and testing sets."""
+    training_samples = list(
+        TrainingKnownSample(KnownSample(**sample))
+        for i, sample in enumerate(samples)
+        if rule(sample, i)
+    )
+    testing_samples = list(
+        TestingKnownSample(KnownSample(**sample))
+        for i, sample in enumerate(samples)
+        if not rule(sample, i)
+    )
+    return training_samples, testing_samples
